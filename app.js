ee45954d632b625c28716c786f069161dc604856
Xabi <ale23@hotmail.fr>
2020-01-08 10:48:05 +0100

    TP2 done

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
+const fs = require('fs');
+const program = require('commander');
+
 const contactFile = process.env.npm_package_config_contacts;
+
+function parseFile(data) {
+  const contacts = new Map();
+
+  JSON.parse(data, (key, value) => {
+    if (Number(key) || key === '0') {
+      console.log(
+        String(contacts.get('lastName')).toUpperCase(),
+        contacts.get('firstName'),
+      );
+    } else {
+      contacts.set(key, value);
+    }
+  });
+}
+
+program
+  .command('list')
+  .description('Show list of contacts')
+  .action((source, destination) => {
+    fs.readFile(contactFile, (err, data) => {
+      if (!err) {
+        parseFile(data);
+      } else {
+        console.log('error :', err);
+      }
+    });
+  });
+// program
+//   .command('add')
+//   .description('Add a conctact to the list of contacts')
+//   .action((source, destination) => {
+//     fs.readFile(contactFile, (err, data) => {
+//       if (!err) {
+//         parseFile(data);
+//       } else {
+//         console.log('error :', err);
+//       }
+//     });
+//   });
+
+program.parse(process.argv);
+
+if (process.argv.length < 3) {
+  program.help();
+}
