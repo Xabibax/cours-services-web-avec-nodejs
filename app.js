6b04db07327e2de16a53001e8737238f7683a177
Xabi <ale23@hotmail.fr>
2020-01-08 13:07:21 +0100

    TP3 done

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
+
 const fs = require('fs');
 const program = require('commander');
+const lib = require('./lib.js');
 
 const contactFile = process.env.npm_package_config_contacts;
 
-function parseFile(data) {
-  const contacts = new Map();
-
-  JSON.parse(data, (key, value) => {
-    if (Number(key) || key === '0') {
-      console.log(
-        String(contacts.get('lastName')).toUpperCase(),
-        contacts.get('firstName'),
-      );
-    } else {
-      contacts.set(key, value);
-    }
-  });
-}
 
 program
   .command('list')
@@ -24,24 +12,56 @@ program
   .action((source, destination) => {
     fs.readFile(contactFile, (err, data) => {
       if (!err) {
-        parseFile(data);
+        lib.listContacts(data);
       } else {
         console.log('error :', err);
       }
     });
   });
-// program
-//   .command('add')
-//   .description('Add a conctact to the list of contacts')
-//   .action((source, destination) => {
-//     fs.readFile(contactFile, (err, data) => {
-//       if (!err) {
-//         parseFile(data);
-//       } else {
-//         console.log('error :', err);
-//       }
-//     });
-//   });
+
+program
+  .command('add')
+  .description('Add a contact to the list of contacts')
+  .action((source, destination) => {
+    if (process.argv.length === 5) {
+      fs.readFile(contactFile, (err, data) => {
+        if (!err) {
+          const contact = new lib.Contact(0, process.argv[4], process.argv[3]);
+          fs.writeFile(contactFile, lib.addContact(data, contact), 'utf8', (err2) => {
+            if (err2) {
+              console.log('error :', err2);
+            }
+          });
+        } else {
+          console.log('error :', err);
+        }
+      });
+    } else {
+      console.log('no contact passed as parameter');
+    }
+  });
+
+program
+  .command('remove')
+  .description('Remove a contact to the list of contacts')
+  .action((source, destination) => {
+    if (process.argv.length === 4) {
+      fs.readFile(contactFile, (err, data) => {
+        if (!err) {
+          fs.writeFile(contactFile, lib.removeContact(data, process.argv[3]), 'utf8', (err2) => {
+            if (err2) {
+              console.log('error :', err2);
+            }
+          });
+        } else {
+          console.log('error :', err);
+        }
+      });
+    } else {
+      console.log('no contact passed as parameter');
+    }
+  });
+
 
 program.parse(process.argv);
 
